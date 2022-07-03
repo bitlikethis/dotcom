b6d78ecc8b623149da2fef277910d7a64f429c4c
Corey McCourt <mccourtrc@gmail.com>
2022-07-03 14:38:42 -0700

    chore: update Home page text

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Container from "components/Container";
 import Prose from "components/Prose";
 import type { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
@@ -16,7 +17,22 @@ const Home: NextPage = () => {
         <Container>
           <Prose>
             <h1>Home</h1>
-            <p>Hello! Welcome to <b>bit like this</b>.</p>
+            <p>
+              Hello! Welcome to{" "}
+              <a>
+                <b>bit like this</b>
+              </a>
+              . My name is Corey, and I like to try new things. I have a
+              curiosity for programming, tech, and art. Take a look at my latest{" "}
+              <Link href="/projects">
+                <a>projects</a>
+              </Link>
+              . You can catch my progress on{" "}
+              <Link href="https://www.youtube.com/channel/UCIUrN4s3vexpbOl-KMqUmqg">
+                <a>Youtube</a>
+              </Link>
+              .
+            </p>
           </Prose>
         </Container>
       </main>
