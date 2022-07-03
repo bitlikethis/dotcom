ec953bbd628f741ff35b226bb6e8416b88e5a70c
Corey McCourt <mccourtrc@gmail.com>
2022-07-03 11:54:34 -0700

    feat: highlight active NavItem

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,30 +1,52 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { ReactNode } from "react";
 import GithubLogo from "./GithubLogo";
 
-const NavItem = ({ href, children }: { href: string; children: ReactNode }) => (
-  <Link href={href}>
-    <a className="group">
-      <span className="inline-block px-4 py-4 hover:text-white hover:bg-primary-900 group-focus:text-white group-focus:bg-primary-900">
-        {children}
-      </span>
-    </a>
-  </Link>
-);
+const NavItem = ({
+  href,
+  children,
+  showActive = false,
+}: {
+  href: string;
+  children: ReactNode;
+  showActive: boolean;
+}) => {
+  const { route } = useRouter();
+  const isActive = route === href;
+  return (
+    <Link href={href}>
+      <a className="group">
+        <span
+          className={`inline-block px-4 py-4 ${
+            showActive && isActive ? "text-white bg-primary-800" : ""
+          } hover:text-white hover:bg-primary-800 group-focus:text-white group-focus:bg-primary-800`}
+        >
+          {children}
+        </span>
+      </a>
+    </Link>
+  );
+};
 
 const Navbar = () => {
   return (
     <nav className="flex justify-between">
       <div>
-        <NavItem href={"/"}>
-          <b>bitlikethis</b>
+        <NavItem href={"/"} showActive={false}>
+          <b>bit like this</b>
         </NavItem>
-        <NavItem href={"/"}>home</NavItem>
-        <NavItem href={"/projects"}>projects</NavItem>
-        <NavItem href={"/blog"}>blog</NavItem>
+        <NavItem href={"/"} showActive>
+          home
+        </NavItem><NavItem href={"/projects"} showActive>
+          projects
+        </NavItem>
+        <NavItem href={"/blog"} showActive>
+          blog
+        </NavItem>
       </div>
       <div className="flex">
-        <NavItem href={"https://github.com/bitlikethis"}>
+        <NavItem href={"https://github.com/bitlikethis"} showActive>
           <GithubLogo />
         </NavItem>
       </div>
