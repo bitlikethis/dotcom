39a7c078ff1d8af6402a8d496c9219e254609380
Corey McCourt <mccourtrc@gmail.com>
2022-07-03 09:40:50 -0700

    chore: change page content to page title

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const Blog: NextPage = () => {
   return (
     <div>
-      <main className="text-2xl">about</main>
+      <main className="text-2xl">blog</main>
     </div>
   );
 };
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const Projects: NextPage = () => {
   return (
     <div>
-      <main className="text-2xl">about</main>
+      <main className="text-2xl">projects</main>
     </div>
   );
 };
