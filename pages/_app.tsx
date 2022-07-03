988ebb8e6f2a6e50bd7b562c9cf0c38f8389c8fc
Corey McCourt <mccourtrc@gmail.com>
2022-07-02 23:38:16 -0700

    style: use slate-900

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import Navbar from "components/Navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="text-primary-800">
+    <div className="text-primary-900">
       <Navbar />
 
       <Component {...pageProps} />
