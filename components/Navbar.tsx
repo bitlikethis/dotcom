7ca911180d00082923127bdc3c2985bc06c1d19d
Corey McCourt <mccourtrc@gmail.com>
2022-07-02 22:42:33 -0700

    fix: remove extra anchor tag in NavLink

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,11 @@ import { ReactNode } from "react";
 
 const NavItem = ({ href, children }: { href: string; children: ReactNode }) => (
   <Link href={href}>
-    <a><span className="inline-block px-4 py-4 hover:text-white hover:bg-black">{children}</span></a>
+    <a>
+      <span className="inline-block px-4 py-4 hover:text-white hover:bg-black">
+        {children}
+      </span>
+    </a>
   </Link>
 );
 
@@ -11,23 +15,13 @@ const Navbar = () => {
   return (
     <nav className="flex justify-between">
       <div>
-        <NavItem href={"/"}>
-          <a>bit like this</a>
-        </NavItem>
+        <NavItem href={"/"}>bit like this</NavItem>
       </div>
       <div>
-        <NavItem href={"/about"}>
-          <a>about</a>
-        </NavItem>
-        <NavItem href={"/projects"}>
-          <a>projects</a>
-        </NavItem>
-        <NavItem href={"/blog"}>
-          <a>contact</a>
-        </NavItem>
-        <NavItem href={"github"}>
-          <a>github</a>
-        </NavItem>
+        <NavItem href={"/about"}>about</NavItem>
+        <NavItem href={"/projects"}>projects</NavItem>
+        <NavItem href={"/blog"}>contact</NavItem>
+        <NavItem href={"github"}>github</NavItem>
       </div>
     </nav>
   );
